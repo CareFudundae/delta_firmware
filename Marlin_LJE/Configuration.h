77b9915889c8a76dc030658e9c0f42e7f1f40707
levisonofed <levi.j.elston@gmail.com>
2017-11-08 21:00:05 -0500

    updated jerk and accel

--- Marlin_LJE/Configuration.h
+++ Marlin_LJE/Configuration.h
@@ -617,7 +617,7 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_ACCELERATION      {5000,5000,5000,1000} // { 9000, 9000, 9000, 10000 }
+#define DEFAULT_MAX_ACCELERATION      {9000,9000,9000,10000}//{5000,5000,5000,1000} // { 9000, 9000, 9000, 10000 }
 
 /**
  * Default Acceleration (change/s) change = mm/s
@@ -628,7 +628,7 @@
  *   M204 T    Travel Acceleration
  */
 #define DEFAULT_ACCELERATION          3000//1000//3000    // X, Y, Z and E acceleration for printing moves
-#define DEFAULT_RETRACT_ACCELERATION  8000//3000    // E acceleration for retracts
+#define DEFAULT_RETRACT_ACCELERATION  3000//3000    // E acceleration for retracts
 #define DEFAULT_TRAVEL_ACCELERATION   3000    // X, Y, Z acceleration for travel (non printing) moves
 
 /**
@@ -639,9 +639,9 @@
  * When changing speed and direction, if the difference is less than the
  * value set here, it may happen instantaneously.
  */
-#define DEFAULT_XJERK                 5.0//20.0
-#define DEFAULT_YJERK                 5.0//20.0
-#define DEFAULT_ZJERK                 5.0//20.0 // Must be same as XY for delta
+#define DEFAULT_XJERK                 20.0//5.0//20.0
+#define DEFAULT_YJERK                 20.0//5.0//20.0
+#define DEFAULT_ZJERK                 20.0//5.0//20.0 // Must be same as XY for delta
 #define DEFAULT_EJERK                 5.0
 
 //===========================================================================
